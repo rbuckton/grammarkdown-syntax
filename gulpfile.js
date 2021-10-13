b333144dc54f2fe846f4a3db5fbec3cb005738a3
Ron Buckton <ron.buckton@microsoft.com>
2021-10-13 14:52:26 -0700

    package with only grammar file

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,28 @@
 const gulp = require("gulp");
-const sourcemaps = require("gulp-sourcemaps");
-const convert = require("gulp-convert");
-const rename = require("gulp-rename");
-const tsb = require("gulp-tsb");
+const yaml = require("js-yaml");
+const plist = require("plist");
+const { Transform } = require("stream");
 const del = require("del");
-const lib = tsb.create("src/lib");
 
-gulp.task("clean:lib", () => del(["out/lib"]));
 gulp.task("clean:syntax", () => del(["out/syntax"]));
-gulp.task("clean", ["clean:lib", "clean:syntax"]);
-
-gulp.task("build:lib", () => lib
-    .src()
-    .pipe(sourcemaps.init())
-    .pipe(lib.compile())
-    .pipe(sourcemaps.write(".", { includeContent: false, destPath: "out/lib" }))
-    .pipe(gulp.dest("out/lib")));
+gulp.task("clean", gulp.task("clean:lib"));
 
 gulp.task("build:syntax", () => gulp
     .src("src/syntax/**/*.yaml")
-    .pipe(convert({ from: "yml", to: "plist" }))
-    .pipe(rename({ extname: ".tmLanguage" }))
+    .pipe(new Transform({
+        objectMode: true,
+        transform: (/** @type {import("vinyl")} */ file, _, cb) => !file.isBuffer() ?
+            cb(new Error("Only buffers are supported")) :
+            cb(null, (
+                file.contents = Buffer.from(plist.build(yaml.load(file.contents.toString("utf8"))), "utf8"),
+                file.extname = ".tmLanguage",
+                file
+            ))
+    }))
     .pipe(gulp.dest("out/syntax")));
 
-gulp.task("build", ["build:lib", "build:syntax"]);
+gulp.task("build", gulp.task("build:syntax"));
 
-gulp.task("watch", ["build"], () => gulp.watch(["src/**/*"], ["build"]));
+gulp.task("watch", gulp.task("build"), () => gulp.watch(["src/**/*"], gulp.task("build")));
 
-gulp.task("default", ["build"]);
+gulp.task("default", gulp.task("build"));
